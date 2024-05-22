aa1d3fa66af44549300f3c68f67081a95db1fcb3
DrenFazlija <dren.fazlija@l3s.de>
2024-05-22 12:09:26 +0200

    Added newest presentation given at SmartCityDays 2024

--- talkmap/org-locations.js
+++ talkmap/org-locations.js
@@ -10,9 +10,9 @@ var addressPoints = [
     -122.16936535498309
   ],
   [
-    "Leibnizhaus, Hannover, Lower Saxony, Germany",
-    52.37143375,
-    9.73222697250177
+    "Ada-und-Theodor-Lessing Volkshochschule, Hannover, Lower Saxony, Germany",
+    52.3727322,
+    9.7311288
   ],
   [
     "Dr. Buhmann Schule, Hannover, Lower Saxony, Germany",
@@ -23,5 +23,10 @@ var addressPoints = [
     "Brainhouse 247, Laatzen, Lower Saxony, Germany",
     52.320846700000004,
     9.786749304728716
+  ],
+  [
+    "Leibnizhaus, Hannover, Lower Saxony, Germany",
+    52.37143375,
+    9.73222697250177
   ]
 ];

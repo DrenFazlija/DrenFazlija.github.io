dc352856de74cba0a5c4e220e757dffeeb904815
Dren Fazlija <dren.fazlija@l3s.de>
2025-01-02 11:55:25 +0100

    New locations (end of 2024)

--- talkmap/org-locations.js
+++ talkmap/org-locations.js
@@ -1,18 +1,8 @@
 var addressPoints = [
   [
-    "St. Nicolai Bothfeld, Hannover, Lower Saxony, Germany",
-    52.413097300000004,
-    9.798551967867008
-  ],
-  [
-    "Gymnasium am Markt, Achim, Lower Saxony, Germany",
-    53.0127139,
-    9.038023245513571
-  ],
-  [
-    "Berufsbildende Schule Burgdorf, Burgdorf, Lower Saxony, Germany",
-    52.43327455,
-    10.013943237123769
+    "Brainhouse 247, Laatzen, Lower Saxony, Germany",
+    52.320846700000004,
+    9.786749304728716
   ],
   [
     "Stanford University, Stanford, California, USA",
@@ -25,18 +15,43 @@ var addressPoints = [
     9.7311288
   ],
   [
+    "Berufsbildende Schule Burgdorf, Burgdorf, Lower Saxony, Germany",
+    52.43327455,
+    10.013943237123769
+  ],
+  [
+    "Gymnasium am Markt, Achim, Lower Saxony, Germany",
+    53.0127139,
+    9.038023245513571
+  ],
+  [
+    "Leibnizhaus, Hannover, Lower Saxony, Germany",
+    52.371425900000006,
+    9.732207529907619
+  ],
+  [
+    "St. Nicolai Bothfeld, Hannover, Lower Saxony, Germany",
+    52.413096249999995,
+    9.798552474600115
+  ],
+  [
     "Dr. Buhmann Schule, Hannover, Lower Saxony, Germany",
     52.3728359,
     9.743968501482346
   ],
   [
-    "Brainhouse 247, Laatzen, Lower Saxony, Germany",
-    52.320846700000004,
-    9.786749304728716
+    "Lange Laube 6, Hannover, Lower Saxony, Germany",
+    52.3762693,
+    9.7307224
   ],
   [
-    "Leibnizhaus, Hannover, Lower Saxony, Germany",
-    52.37143375,
-    9.73222697250177
+    "Barbarastra\u00dfe 7, Dortmund, Germany",
+    51.56861965,
+    7.376001042899669
+  ],
+  [
+    "Friedrich-Paffrath-Stra\u00dfe 101, Wilhelmshaven, Germany",
+    53.5480146,
+    8.088280012905999
   ]
 ];
